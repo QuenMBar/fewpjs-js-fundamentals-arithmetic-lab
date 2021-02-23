91f6e928ed842ff65a7602521cc33c5258c548bc
QuenMBar <Qbeast360@gmail.com>
2021-02-22 17:25:13 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
+let num3 = 25;
+let num4 = 7;
 
-// Write your code here
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10);
+
+let mod = num3 % num4;
+
+let max = Math.max(1, 5, 15, 20, 12, 4);
